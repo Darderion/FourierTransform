269156a853e5234d5a9d5f28410e3ccda92679d8
Darderion <Eobard1SA@gmail.com>
2019-11-15 01:28:40 +0300

    Added RecoverSignal
    Implemented IDFT

--- LabAssignment.asm
+++ LabAssignment.asm
@@ -199,8 +199,7 @@ CalculateSpectrum PROC	; [RCX] - Spectrum
 	fstp dword ptr [RCX +	(6 + 8) * 4	]
 
 		; storing sqrt(2) / 2 in ST(0)
-	mov R15, 2
-	mov dword ptr [RCX + 1 * 4], R15d
+	mov dword ptr [RCX + 1 * 4], 2
 	fild dword ptr [RCX + 1 * 4]
 	fsqrt
 	fild dword ptr [RCX + 1 * 4]
@@ -311,6 +310,283 @@ CalculateSpectrum ENDP
 ; -------------------------------------------------------------------------------------	;
 RecoverSignal PROC	; [RCX] - Signal
 					; [RDX] - Spectrum
+	sub RSP, (8 * 2) * 8; allocating space for 8 complex numbers
+
+	finit
+	fld dword ptr [RDX				]	; loading Re(x[0])
+	fld dword ptr [RDX + 4 * 4		]	; loading Re(x[4])
+	fld ST(1)							; ST0 = Re(x[0])
+	fadd ST(0), ST(1)					; calculating Re(e[0]) = Re(x[0]) + Re(x[4])
+	fld ST(2)							; ST0 = Re(x[0])
+	fsub ST(0), ST(2)					; calculating Re(e[1]) = Re(x[0]) - Re(x[4])
+	fstp dword ptr [RSP + 1 * 4	]		; storing Re(e[1])
+	fstp dword ptr [RSP			]		; storing Re(e[0])
+	finit								; clearing ST
+
+		; Im(e[0]), Im(e[1]) are optimized out
+
+	fld dword ptr [RDX + 2 * 4		]	; loading Re(x[2])
+	fld dword ptr [RDX + 6 * 4		]	; loading Re(x[6])
+	fld ST(1)							; ST0 = Re(x[2])
+	fadd ST(0), ST(1)					; calculating Re(o[0]) = Re(x[2]) + Re(x[6])
+		; calculating Re(o[1]) = Re(x[2]) - Re(x[6])
+		; o[1] * w(1, n) = o[1] * (-i)
+		; Re(x[2]) - Re(x[6]) = -Im(o[1])
+		; Im(o[1]) is therefore optimized out
+	fstp dword ptr [RSP + 2 * 4 ]		; storing Re(o[0])
+	finit								; clearing ST
+	
+	fld dword ptr [RDX + (2 + 8) * 4 ]	; loading Im(x[2])
+	fld dword ptr [RDX + (6 + 8) * 4 ]	; loading Im(x[6])
+	fld ST(1)
+		; calculating Im(o[1]) = Im(x[2]) - Im(x[6])
+		; o[1] * w(1, n) = o[1] * (-i)
+		; Im(x[2]) - Im(x[6]) = Re(o[1])
+	fsub ST(0), ST(2)
+	fstp dword ptr [RSP + 3 * 4		  ]	; storing Re(o[1])
+	finit								; clearing ST
+
+		; [RSP + x * 4] + [RSP + (x + 8) * 4]*i :
+		;	x = 0 -> e[0]
+		;	x = 1 -> e[1]
+		;	x = 2 -> o[0]
+		;	x = 3 -> o[1]
+
+	fld dword ptr [RSP		 ]		; loading Re(e[0])
+	fld dword ptr [RSP + 2 * 4 ]	; loading Re(o[0])
+	fld ST(1)						; ST0 = Re(e[0])
+	fadd ST(0), ST(1)				; calculating Re(E[0])
+	fld ST(2)						; ST0 = Re(e[0])
+	fsub ST(0), ST(2)				; calculating Re(E[2])
+	fstp dword ptr [RSP + 2 * 4 ]	; storing Re(E[2])
+	fstp dword ptr [RSP			]	; storing Re(E[0])
+	finit
+	
+	fld dword ptr [RSP + 1 * 4 ]	; loading Re(e[1])
+	fld dword ptr [RSP + 3 * 4 ]	; loading Re(o[1])
+	fld ST(1)						; ST0 = Re(e[1])
+	fadd ST(0), ST(1)				; calculating Re(E[1])
+	fld ST(2)						; ST0 = Re(e[1])
+	fsub ST(0), ST(2)				; calculating Re(E[3])
+	fstp dword ptr [RSP + 3 * 4 ]	; storing Re(E[3])
+	fstp dword ptr [RSP + 1 * 4 ]	; storing Re(E[1])
+	finit
+
+		; Im(E[0..3]) have no effect on Re(X[k]) and are optimized out
+	
+		; processing odd indexes
+	finit
+	fld dword ptr [RDX + 1 * 4		]	; loading Re(x[1])
+	fld dword ptr [RDX + 5 * 4		]	; loading Re(x[5])
+	fld ST(1)							; ST0 = Re(x[1])
+	fadd ST(0), ST(1)					; calculating Re(e[0]) = Re(x[1]) + Re(x[5])
+	fld ST(2)							; ST0 = Re(x[1])
+	fsub ST(0), ST(2)					; calculating Re(e[1]) = Re(x[1]) - Re(x[5])
+	fstp dword ptr [RSP + 5 * 4	]		; storing Re(e[1])
+	fstp dword ptr [RSP + 4 * 4]		; storing Re(e[0])
+	finit								; clearing ST
+
+	fld dword ptr [RDX + (1 + 8) * 4 ]	; loading Im(x[1])
+	fld dword ptr [RDX + (5 + 8) * 4 ]	; loading Im(x[5])
+	fld ST(1)							; ST0 = Im(x[1])
+	fadd ST(0), ST(1)					; calculating Im(e[0]) = Im(x[1]) + Im(x[5])
+	fld ST(2)							; ST0 = Im(x[1])
+	fsub ST(0), ST(2)					; calculating Im(e[1]) = Im(x[1]) - Im(x[5])
+	fstp dword ptr [RSP + (5 + 8) * 4 ] ; storing Im(e[1])
+	fstp dword ptr [RSP + (4 + 8) * 4 ]	; storing Im(e[0])
+	finit								; clearing ST
+
+	fld dword ptr [RDX + 3 * 4		]	; loading Re(x[3])
+	fld dword ptr [RDX + 7 * 4		]	; loading Re(x[7])
+	fld ST(1)							; ST0 = Re(x[3])
+	fadd ST(0), ST(1)					; calculating Re(o[0]) = Re(x[3]) + Re(x[7])
+	fld ST(2)							; ST0 = Re(x[7])
+		; calculating Re(o[1]) = Re(x[3]) - Re(x[7])
+		; o[1] * w(1, n) = o[1] * (-i)
+		; Re(x[3]) - Re(x[7]) = -Im(o[1]*w)
+		; Reverse butterfly method -> Im(o[1]*w) = Im(x[3]) - Im(x[7])
+	fsub ST(0), ST(2)
+	fstp dword ptr [RSP + (7 + 8) * 4 ]	; storing Im(o[1]*w)
+	fstp dword ptr [RSP + 6 * 4 ]		; storing Re(o[0]*w)
+	finit								; clearing ST
+	
+	fld dword ptr [RDX + (3 + 8) * 4 ]	; loading Im(x[3])
+	fld dword ptr [RDX + (7 + 8) * 4 ]	; loading Im(x[7])
+	fld ST(1)							; ST0 = Im(x[3])
+	fadd ST(0), ST(1)					; calculating Im(o[0]) = Im(x[3]) + Im(x[7])
+	fld ST(1)							; ST0 = Im(x[7])
+		; calculating Im(o[1]) = Im(x[3]) - Im(x[7])
+		; o[1] * w(1, n) = o[1] * (-i)
+		; Im(x[3]) - Im(x[7]) = Re(o[1]*w)
+		; Reverse butterfly method -> Re(o[1]*w) = Im(x[7]) - Im(x[3])
+	fsub ST(0), ST(3)
+	fstp dword ptr [RSP + 7 * 4		  ]	; storing Re(o[1]*w)
+	fstp dword ptr [RSP + (6 + 8) * 4 ]	; storing Im(o[0]*w)
+	finit								; clearing ST
+
+		; [RSP + x * 4] + [RSP + (x + 8) * 4]*i :
+		;	x = 4 -> e[0]
+		;	x = 5 -> e[1]
+		;	x = 6 -> o[0]*w
+		;	x = 7 -> o[1]*w
+
+	fld dword ptr [RSP + 4 * 4 ]	; loading Re(e[0])
+	fld dword ptr [RSP + 6 * 4 ]	; loading Re(o[0])
+	fld ST(1)						; ST0 = Re(e[0])
+	fadd ST(0), ST(1)				; calculating Re(O[0])
+	fstp dword ptr [RSP + 4 * 4 ]	; storing Re(O[0])
+	finit
+		; O[0] is stored
+	
+	fld dword ptr [RSP + 5 * 4 ]	; loading Re(e[1])
+	fld dword ptr [RSP + 7 * 4 ]	; loading Re(o[1])
+	fld ST(1)						; ST0 = Re(e[1])
+	fadd ST(0), ST(1)				; calculating Re(O[1])
+	fld ST(2)						; ST0 = Re(e[1])
+	fsub ST(0), ST(2)				; calculating Re(O[3])
+	fstp dword ptr [RSP + 7 * 4 ]	; storing Re(O[3])
+	fstp dword ptr [RSP + 5 * 4 ]	; storing Re(O[1])
+	finit
+		; Re(O[1]) and Re(O[3])
+		; Im(O[2]) * (-i) = (e[0] - o[0]) * (-i) = Re(O[2]*w)
+		; Reverse butterfly method -> Re(O[2]*w) = o[0] - e[0]
+	fld dword ptr [RSP + (4 + 8) * 4 ]	; loading Im(e[0])
+	fld dword ptr [RSP + (6 + 8) * 4 ]	; loading Im(o[0])
+	fld ST(0)							; ST0 = Im(o[0])
+	fsub ST(0), ST(2)					; calculating Re(O[2])
+	fstp dword ptr [RSP + 6 * 4 ]		; storing Re(O[2])
+	finit
+	
+	fld dword ptr [RSP + (5 + 8) * 4 ]	; loading Im(e[1])
+	fld dword ptr [RSP + (7 + 8) * 4 ]	; loading Im(o[1])
+	fld ST(1)							; ST0 = Im(e[1])
+	fadd ST(0), ST(1)					; calculating Im(O[1])
+	fld ST(2)							; ST0 = Im(e[1])
+	fsub ST(0), ST(2)					; calculating Im(O[3])
+	fstp dword ptr [RSP + (7 + 8) * 4 ]	; storing Im(O[3])
+	fstp dword ptr [RSP + (5 + 8) * 4 ]	; storing Im(O[1])
+
+		; Stack:
+		; [RSP + x * 4] + [RSP + (x + 8) * 4] :
+		;	[0..3] -> E[0..3]
+		;	4 -> O[0]
+		;	5 -> O[1] / W(1, 8)
+		;	6 -> Re(O[2])
+		;	7 -> O[3] / W(3, 8)
+
+		; storing sqrt(2) / 2 in ST(0)
+	finit
+	mov dword ptr [RCX + 1 * 4 ], 2
+	fild dword ptr [RCX + 1 * 4]
+	fsqrt
+	fild dword ptr [RCX + 1 * 4]
+	fdivp
+	fld dword ptr [RSP + (5 + 8) * 4]	; loading Im(O[1])
+	fld dword ptr [RSP + 5 * 4]			; loading Re(O[1])
+		; multiplying O[1] by W(1, 8) is the same as calculating
+		;	Re(O[1]*w) = Im(O[1]) + Re(O[1])
+		;	Im(O[1]*w) = Im(O[1]) - Re(O[1])- optimized out
+		; Reverse butterfly method -> Re(O[1] * (1 + i)) = -Im(O[1]) + Re(O[1])
+	fsub ST(0), ST(1)					; calculating Re(O[1]*w)
+	fmul ST(0), ST(2)
+	fstp dword ptr [RSP + 5 * 4]		; storing Re(O[1]*w)
+	fstp ST(0)
+	
+	fld dword ptr [RSP + 7 * 4]			; loading Re(O[3])
+	fld dword ptr [RSP + (7 + 8) * 4]	; loading Im(O[3])
+		; multiplying O[3] by W(3, 8) is the same as calculating
+		;	Re(O[3]*w) = Im(O[3]) - Re(O[3])
+		;	Im(O[3]*w) = Im(O[3]) + Re(O[3])- optimized out
+		; Reverse butterfly method -> Re(O[3] * (i - 1)) = -Im(O[1]) - Re(O[1])
+	fldz
+	fsub ST(0), ST(1)
+	fsub ST(0), ST(2)					; calculating Re(O[3]*w)
+	fmul ST(0), ST(3)
+	fstp dword ptr [RSP + 7 * 4]		; storing Re(O[3]*w)
+	
+	finit
+	fld1				; ST(0) = 1
+	push 0				; M(0) = 8
+	mov dword ptr [RSP], 8;
+	fild dword ptr [RSP];
+	fdivp ST(1), ST(0)	; ST(0) = 1 / 8
+
+	fld dword ptr [RSP + 1 * 4 + 4]	; loading E[0]
+	fld dword ptr [RSP + 5 * 4 + 4]	; loading O[0]
+	fld ST(1)						; ST(0) = E[0]
+	fadd ST(0), ST(1)				; X[0] = E[0] + O[0]
+	fmul ST(0), ST(3)				; X[0] * (1 / N)
+	fistp dword ptr [RSP]			; storing Re(X[0])
+	mov R8b, byte ptr [RSP]			;
+	mov byte ptr [RCX + 7], R8b			;
+	fld ST(1)						; ST(0) = E[0]
+	fsub ST(0), ST(1)				; X[4] = E[0] - O[0]
+	fmul ST(0), ST(3)				; X[4] * (1 / N)
+	fistp dword ptr [RSP]			; storing Re(X[4])
+	mov R8b, byte ptr [RSP]			;
+	mov byte ptr [RCX + 4], R8b		;
+	fstp ST(0)
+	fstp ST(0)
+
+	fld dword ptr [RSP + 2 * 4 + 4]	; loading E[1]
+	fld dword ptr [RSP + 6 * 4 + 4]	; loading O[1]
+	fld ST(1)						; ST(0) = E[1]
+	fadd ST(0), ST(1)				; X[1] = E[1] + O[1]
+	fmul ST(0), ST(3)				; X[1] * (1 / N)
+	fistp dword ptr [RSP]			; storing Re(X[1])
+	mov R8b, byte ptr [RSP]			;
+	mov byte ptr [RCX + 1], R8b		;
+	fld ST(1)						; ST(0) = E[1]
+	fsub ST(0), ST(1)				; X[5] = E[1] - O[1]
+	fmul ST(0), ST(3)				; X[5] * (1 / N)
+	fistp dword ptr [RSP]			; storing Re(X[5])
+	mov R8b, byte ptr [RSP]			;
+	mov byte ptr [RCX + 5], R8b		;
+	fstp ST(0)
+	fstp ST(0)
+
+	fld dword ptr [RSP + 3 * 4 + 4]	; loading E[2]
+	fld dword ptr [RSP + 7 * 4 + 4]	; loading O[2]
+	fld ST(1)						; ST(0) = E[2]
+	fadd ST(0), ST(1)				; X[2] = E[2] + O[2]
+	fmul ST(0), ST(3)				; X[2] * (1 / N)
+	fistp dword ptr [RSP]			; storing Re(X[2])
+	mov R8b, byte ptr [RSP]			;
+	mov byte ptr [RCX + 2], R8b		;
+	fld ST(1)						; ST(0) = E[2]
+	fsub ST(0), ST(1)				; X[6] = E[2] - O[2]
+	fmul ST(0), ST(3)				; X[6] * (1 / N)
+	fistp dword ptr [RSP]			; storing Re(X[6])
+	mov R8b, byte ptr [RSP]			;
+	mov byte ptr [RCX + 6], R8b		;
+	fstp ST(0)
+	fstp ST(0)
+
+	fld dword ptr [RSP + 4 * 4 + 4]	; loading E[3]
+	fld dword ptr [RSP + 8 * 4 + 4]	; loading O[3]
+	fld ST(1)						; ST(0) = E[3]
+	fadd ST(0), ST(1)				; X[3] = E[3] + O[3]
+	fmul ST(0), ST(3)				; X[3] * (1 / N)
+	fistp dword ptr [RSP]			; storing Re(X[3])
+	mov R8b, byte ptr [RSP]			;
+	mov byte ptr [RCX + 3], R8b		;
+	fld ST(1)						; ST(0) = E[3]
+	fsub ST(0), ST(1)				; X[7] = E[3] - O[3]
+	fmul ST(0), ST(3)				; X[7] * (1 / N)
+	fistp dword ptr [RSP]			; storing Re(X[7])
+	mov R8b, byte ptr [RSP]			;
+	mov byte ptr [RCX + 7], R8b		;
+
+	; fld ST(0)
+	; fsub ST(0), ST(2)
+	; fmul ST(0), ST(3)
+	; fistp dword ptr [RCX + 4]
+	; fstp ST(0)
+	; fstp ST(0)
+	; push 1
+
+	add RSP, (8 * 2) * 8 + 8;
+
 	ret
 RecoverSignal ENDP
 END
